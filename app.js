621853c0b498b220891cfdd8d754d3676e2e81ce
nashraghaffar <nashraghaffar08@gmail.com>
2020-06-07 17:26:15 -0700

    ready

---
+++ app.js
@@ -0,0 +1,255 @@
+
+
+// // CHAPTER 01
+
+// // NO. 01
+// alert("Hello!! Welcome to our Website");
+
+// // NO. 02
+
+// alert("Error! Please enter a valid password");
+
+// // NO. 03
+// alert("Welcome to JS Land... \nHappy Coding!");
+
+
+// // NO. 04
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+
+// // NO. 05
+// alert("Hello.. I can rus JS through my web browser's console");
+
+
+
+
+
+
+
+
+// // CHAPTER 02
+
+
+// // No. 01
+// var username;
+
+
+
+// // No. 02
+// var myName="Nashra Ghaffar";
+
+
+
+// // No. 03
+// var message;
+// message="Hello World";
+// alert(message);
+
+// // No. 04
+
+// var name="Nashra";
+// var age="15";
+// var app="Certified Mobile Application Development";
+// alert(name);
+// alert(age);
+// alert(app);
+
+// // No. 05
+
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+
+// // No. 06
+
+
+// var email="nashraghaffar08@gmail.com";
+// alert("My email address is" + " " + email);
+
+
+// // No. 07
+
+
+// var book="A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + " "+book);
+
+// // No. 08
+
+// document.write("Yah! I can write HTML content through JavaScript");
+
+
+// // No. 09
+
+// var design=" “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” ";
+// alert(design);
+
+                                                     // END
+
+
+
+
+
+
+
+
+
+
+
+                                                  // CHAPTER 03
+
+
+
+ // No. 01
+
+// var age=15;
+// alert("My age is" +" " +age);
+
+
+// // No. 02
+// var visits=12;
+// alert("You have visited this webpage"+" "+visits+" "+"times");
+
+
+
+// // No. 03
+// var birthYear="2005";
+// document.write("\nMy birth year is "+birthYear);
+// document.write("\nDatatype of my declared variable is number");
+
+
+//No. 04
+
+// var name = prompt("Enter your name", "Your name here");
+// var product= prompt("What you want to order??" , "T-shirts,shoes etc..");
+// var quantity= +prompt("Quantity of your product ??", "2,3,4,...");
+// document.write(name+" "+"ordered"+" "+quantity+" "+product+" "+"on XYZ Clothing store");
+
+// END
+
+
+
+
+
+
+                                                // CHAPTER 04
+
+//no. 01
+
+// var variable1, variable2, variable3 ; 
+
+//no. 02
+
+
+// // LEGAL
+// var oppo$;
+// var student12;
+// var workers;
+// var smart00;
+// var _Full;
+
+// //ILLEGAL
+// var 3man;
+// var stylo+;
+// var -Full;
+// var ?Data;
+// var done@;
+
+// document.write("Rules for naming JS variables");
+// document.write("Variables names can only contain numbers,$, and _ . For Example  $my_1stVariable");
+
+// document.write("Variables must begin with a letter,$ or _. For example $name, _name or name");
+// document.write("Variable names are case sensitive");
+// document.write("Variable names should not be JS keywords");
+
+
+                                                    // END
+
+                                                    // CHAPTER 05
+                        
+
+// No. 01
+
+                        // ADDITION
+
+// var num1= +prompt("Enter first number");
+// var num2= +prompt("Enter second number");
+// var sum = num1 + num2;
+// document.write("The sum of" + " " + num1+" "+ "and"+" "+num2+" "+"is" +" "+sum);
+
+// No. 02
+
+
+                        // SUBTRACTION
+
+// var num1= +prompt("Enter first number");
+// var num2= +prompt("Enter second number");
+// var difference = num1 - num2;
+// document.write("The difference of" + " " + num1+" "+ "and"+" "+num2+" "+"is" +" "+difference);
+
+                    // MULTIPLICATION
+
+//  var num1= +prompt("Enter first number");
+//  var num2= +prompt("Enter second number");
+//  var product = num1 * num2;
+//  document.write("The product of" + " " + num1+" "+ "and"+" "+num2+" "+"is" +" "+product);
+
+
+                            // DIVISION
+
+
+// var num1= +prompt("Enter first number");
+// var num2= +prompt("Enter second number");
+// var division = num1 / num2;
+// document.write("The division of" + " " + num1+" "+ "and"+" "+num2+" "+"is" +" "+division);
+
+
+                                        // MODULUS
+
+// var num1= +prompt("Enter first number");
+// var num2= +prompt("Enter second number");
+// var mod = num1 % num2;
+// document.write("The modulus of" + " " + num1+" "+ "and"+" "+num2+" "+"is" +" "+mod);
+
+
+// No. 03
+
+// var num;
+// document.write("\r\nThe value of variable after declaration is undefined");
+
+// var num=5;
+// document.write("\r\nThe value of variable after initialization is:" + " " + num);
+
+// num++;
+// document.write("\r\nThe value of variable after increament is:" + " " + num);
+
+// num=num + 7;
+// document.write("\r\nThe value of variable after adding 7 is:" + " " + num);
+
+// num--;
+// document.write("\r\nThe value of variable after decreament is:" + " " + num);
+
+// num=num%3;
+// document.write("\r\nThe remainder is:" + " " + num);
+
+//No. 04
+
+// var price=600;
+// var buying=5;
+// totalcost=price * buying;
+// document.write("Total Cost to buy"+" "+buying+" "+"tickets to a movie is"+" "+totalcost+" "+"PKR");
+
+
+//No. 05
+
+// var num=4;
+// document.write(num+" "+"x"+" "+1+" "+"="+" "+ num*1);
+// document.write("<br>"+num+" "+"x"+" "+2+" "+"="+" "+ num*2);
+// document.write("<br>"+num+" "+"x"+" "+3+" "+"="+" "+ num*3);
+// document.write("<br>"+num+" "+"x"+" "+4+" "+"="+" "+ num*4);
+// document.write("<br>"+num+" "+"x"+" "+5+" "+"="+" "+ num*5);
+// document.write("<br>"+num+" "+"x"+" "+6+" "+"="+" "+ num*6);
+// document.write("<br>"+num+" "+"x"+" "+7+" "+"="+" "+ num*7);
+// document.write("<br>"+num+" "+"x"+" "+8+" "+"="+" "+ num*8);
+// document.write("<br>"+num+" "+"x"+" "+9+" "+"="+" "+ num*9);
+// document.write("<br>"+num+" "+"x"+" "+10+" "+"="+" "+ num*10);
+
